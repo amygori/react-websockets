5c04456a0483c8a73120573f86e825383fed1a92
Amy Gori <amydgori@gmail.com>
2023-03-05 15:29:20 -0500

    Add links to chat rooms

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 const ChatList = () => {
-  const chats = ["red", "blue", "yellow"]
+  const chatRooms = ["red", "blue", "yellow"]
   return (
     <div className="dev-list">
           <ul>
-            {chats.map(chat => <li>{chat}</li>)}
+            {chatRooms.map(chatRoom => <li><Link to={`/chat/${chatRoom}`}>{chatRoom}</Link></li>)}
           </ul>
         </div>
   )
